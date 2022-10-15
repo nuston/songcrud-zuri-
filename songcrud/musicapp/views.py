fc359cda2fb40b788020fda2dae285fb337c9bd6
nuston_pay <43966559+nuston@users.noreply.github.com>
2022-10-15 11:33:12 +0100

    songcrud
    a song crud project which has a music app

---
+++ songcrud/musicapp/views.py
@@ -0,0 +1,9 @@
+rom django.shortcuts import render
+
+# Create your views here.
+
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the musicapp index.")
